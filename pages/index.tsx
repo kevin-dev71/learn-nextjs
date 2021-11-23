a33a7cfe2f8d7348ec51b0b12710a8f71e9cbf24
Kevin Dev <kevin.developer71@gmail.com>
2021-11-22 21:21:44 -0600

    Typescript added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Head from "next/head"
 import Layout, { siteTitle } from "../components/layout"
-// @ts-ignore
 import utilStyles from "../styles/utils.module.css"
 import { getSortedPostsData } from "../lib/posts"
 import Link from "next/link"
 import Date from "../components/date"
+import { GetStaticProps } from "next"
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
@@ -15,7 +15,15 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
